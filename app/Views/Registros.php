504b9489ffebebcb9ef26430464a5818901c54b5
JoanaHdez <ciahdez22@gmail.com>
2025-12-30 18:13:00 -0600

    v-301225-1812

--- app/Views/Registros.php
+++ app/Views/Registros.php
@@ -62,7 +62,6 @@
                         <?php if (session()->getFlashdata('error')): ?>
                         <div class="alert alert-danger alert-dismissible fade show" role="alert">
                             <?= session()->getFlashdata('error') ?>
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                         </div>
                         <?php endif; ?>
 
